e961c4d58eb9bec52ade0a9e30c8216a86ac212b
Julia <julia.golovko23@gmail.com>
2022-01-22 20:23:30 +0200

    Add Logo in Not Found component

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Logo from '../Logo';
 import styles from './NotFound.module.sass';
 
 const NotFound = () => (
-  <div className={styles.container}><span>Not Found</span></div>
+  <div className={styles.container}>
+  <Logo />
+  <div className={styles.textContainer}><span>Not Found</span></div>
+  </div>
 );
 
 export default NotFound;
